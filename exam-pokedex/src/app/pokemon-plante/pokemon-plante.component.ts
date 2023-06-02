0bd6d5000ab71be4822b09ecc33c8eed6d64e45c
scaphydata <scaphydata@scaphydata.com>
2023-06-02 14:59:30 +0200

    commit 1.1 exam-pokemon

---
+++ exam-pokedex/src/app/pokemon-plante/pokemon-plante.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-plante',
+  templateUrl: './pokemon-plante.component.html',
+  styleUrls: ['./pokemon-plante.component.scss']
+})
+export class PokemonPlanteComponent {
+
+}
