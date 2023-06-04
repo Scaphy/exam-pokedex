2cb68e3ac574330012e75dc9c0fd7afa6e66f8ad
scaphydata <scaphydata@scaphydata.com>
2023-06-04 22:52:50 +0200

    pokedex liste

--- exam-pokedex/src/app/app.module.ts
+++ exam-pokedex/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { PokemonFeuComponent } from './pokemon-feu/pokemon-feu.component';
 import { PokemonFoudreComponent } from './pokemon-foudre/pokemon-foudre.component';
 import { PokemonPlanteComponent } from './pokemon-plante/pokemon-plante.component';
+import { PokemeonFeuListComponent } from './pokemeon-feu-list/pokemeon-feu-list.component';
+import { PokemeonPlanteListComponent } from './pokemeon-plante-list/pokemeon-plante-list.component';
+import { PokemeonFoudreListComponent } from './pokemeon-foudre-list/pokemeon-foudre-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokemonFeuComponent,
     PokemonFoudreComponent,
-    PokemonPlanteComponent
+    PokemonPlanteComponent,
+    PokemeonFeuListComponent,
+    PokemeonPlanteListComponent,
+    PokemeonFoudreListComponent
   ],
   imports: [
     BrowserModule,
---
+++ exam-pokedex/src/app/pokemeon-feu-list/pokemeon-feu-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemeon-feu-list',
+  templateUrl: './pokemeon-feu-list.component.html',
+  styleUrls: ['./pokemeon-feu-list.component.css']
+})
+export class PokemeonFeuListComponent {
+
+}
