e009eda43b22bc2cfa2476a32068ef70a8633f1c
scaphydata <scaphydata@scaphydata.com>
2023-06-05 00:24:12 +0200

    exam pokemon

--- exam-pokedex/src/app/pokemeon-feu-list/pokemeon-feu-list.component.ts
+++ exam-pokedex/src/app/pokemeon-feu-list/pokemeon-feu-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 /* import Hero from '../types/hero.type'; */
-import Pokemon from '../types/pokemon.type';
+import Pokemon from '../types/pokemon_feu.type';
 
 @Component({
   selector: 'app-pokemeon-feu-list',
@@ -18,7 +18,7 @@ export class PokemeonFeuListComponent implements OnInit {
 constructor() { }
 ngOnInit() {
 }
-pokemonsfeu : Pokemon [] = [
+pokemonsfeu : Pokemon_Feu [] = [
   { id:1,nom:'Felinferno',taille:150,evolution:10,faiblesse:'peur_de_l_eau',attaque:'fondre_glace' },
   { id:2,nom:'Salameche',taille:200,evolution:15,faiblesse:'peur_neige',attaque:'depart_feu' },
   { id:3,nom:'Repincel',taille:250,evolution:20,faiblesse:'humidite',attaque:'rapide' },
