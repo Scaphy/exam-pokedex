688f965aa2c7f3015a3b06c001eadd2219c01ab5
scaphydata <scaphydata@scaphydata.com>
2023-06-06 20:03:21 +0200

    pokemon

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PokemonFeuComponent } from './pokemon-feu/pokemon-feu.component';
+import { PokemonFoudreComponent } from './pokemon-foudre/pokemon-foudre.component';
+import { PokemonPlanteComponent } from './pokemon-plante/pokemon-plante.component';
+import { PokemeonFeuListComponent } from './pokemeon-feu-list/pokemeon-feu-list.component';
+import { PokemeonPlanteListComponent } from './pokemeon-plante-list/pokemeon-plante-list.component';
+import { PokemeonFoudreListComponent } from './pokemeon-foudre-list/pokemeon-foudre-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokemonFeuComponent,
+    PokemonFoudreComponent,
+    PokemonPlanteComponent,
+    PokemeonFeuListComponent,
+    PokemeonPlanteListComponent,
+    PokemeonFoudreListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+/* @NgModule({
+declarations: [
+AppComponent,
+HeroesListComponent // cette ligne a été ajoutée suite à notre commande
+],
+imports: [
+BrowserModule,
+AppRoutingModule
+],
+providers: [],
+bootstrap: [AppComponent]
+})
+export class AppModule { } */
---
+++ src/app/pokemon-feu/pokemon-feu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-feu',
+  templateUrl: './pokemon-feu.component.html',
+  styleUrls: ['./pokemon-feu.component.css']
+})
+export class PokemonFeuComponent {
+
+}
